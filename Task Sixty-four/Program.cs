614c43723d8700e610463236b8e24cf6624dd363
Yaroslav Shishkin <company-rosstan@yandex.ru>
2023-06-21 21:57:39 +0300

    sixty-four

---
+++ Task Sixty-four/Program.cs
@@ -0,0 +1,20 @@
+﻿// Задача 64: Задайте значение N. Напишите программу, которая выведет все натуральные числа
+//  в промежутке от N до 1. Выполнить с помощью рекурсии.
+
+Console.Clear();
+Console.Write("Введите число: ");
+int number = int.Parse(Console.ReadLine()!);
+Main(args);
+void Print(int i)
+{
+    if (i >= number)
+    return;
+    i++;
+    Print(i);
+    Console.Write($"{i}, ");
+}
+
+void Main(string[] args)
+{
+    Print(0);
+}
